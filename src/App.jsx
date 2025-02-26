7ea143610eca6a7d8a5bede80fa2b403492f2a1e
diegolenci <lencidiego0@gmail.com>
2025-02-26 10:25:30 +0100

    Add: array film + import useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useState, useEffect } from "react"
 
+const initialMovies = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+];
 
 function App() {
   
+  const [filteredMovies, setFilteredMovies] = useState(initialMovies);
+  const [genre, setGenre] = useState('')
 
   return (
     <>
